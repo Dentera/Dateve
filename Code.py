6641f0bcd823d6c67da5a3b64afb334020b861ae
Artem <artem.loskutov@gmail.com>
2023-12-13 22:48:46 +0300

    Added event sorting

--- Code.py
+++ Code.py
@@ -5,7 +5,7 @@ from code_separation.UI import *  # Подключение всех модуле
 from code_separation.Datachecker import data_check \
     # Подключение функции data_check из файла, который в папке code_separation.
 
-bot = telebot.TeleBot("5205782024:AAHX0lbe_qQttERiBl3Wx4otcSnqCQMHW4k")  \
+bot = telebot.TeleBot("")  \
     # Подключение токена телеграм-бота для работы самим ботом.
 
 adding_year = adding_month = adding_day = adding_event = False \
@@ -13,7 +13,7 @@ adding_year = adding_month = adding_day = adding_event = False \
 adding_event_year = adding_event_month = adding_event_day = int() \
     # Создание переменных типа integer, для сохранения числовых значений года/месяца/дня.
 
-db = sqlite3.connect('database/Data.db', check_same_thread=False)  \
+db = sqlite3.connect("database/Data.db", check_same_thread=False)  \
     # Создание переменной db, содержащей в себе ссылку на базу данных Data с включённой многопоточностью.
 cursor = db.cursor()  # Создание переменной cursor для работы с базой данных.
 
@@ -23,18 +23,18 @@ def db_add(chat_id, event_day, event):
     VALUES(?, ?, ?)""", (chat_id, event_day, event))
 
 
-@bot.message_handler(commands=['start'])
+@bot.message_handler(commands=["start"])
 def start(message):
     bot.send_message(message.chat.id, "Здравствуйте, я прототип календарь бота Dateve.")
     bot.send_message(message.chat.id, "Для получения руководства использования нажмите на кнопку 'Команды'",
                      reply_markup=keyboard_start)
 
 
-@bot.message_handler(content_types=['text'])
+@bot.message_handler(content_types=["text"])
 def handler(message):
     global adding_year, adding_month, adding_event, adding_event_year, adding_event_month, adding_event_day
     text = message.text
-    if text == 'Dateve':
+    if text == "Dateve":
         bot.send_message(message.chat.id, "Готов к работе", reply_markup=keyboard_call)
     if adding_year is True:
         adding_event_year = int(text)
@@ -84,9 +84,14 @@ def callback(call):
         adding_event = True
     if call.data == "output":
         bot.delete_message(call.message.chat.id, call.message.message_id)
-        out = cursor.execute("""SELECT event,event_day FROM events WHERE chatID = (?)""", (call.message.chat.id,)).fetchall()
+        out = cursor.execute("""SELECT event,event_day FROM events WHERE chatID = (?)""",
+                             (call.message.chat.id,)).fetchall()
         if len(out) > 0:
             bot.send_message(call.message.chat.id, "Вывод событий:", reply_markup=keyboard_call)
+            for x in range(len(out) - 1):
+                for y in range(len(out) - x - 1):
+                    if out[y][1] > out[y + 1][1]:
+                        out[y], out[y + 1] = out[y + 1], out[y]
             for i in range(len(out)):
                 send = ' '.join(out[i])
                 bot.send_message(call.message.chat.id, send)
@@ -94,5 +99,5 @@ def callback(call):
             bot.send_message(call.message.chat.id, "Сохранённых событий в этом чате нет", reply_markup=keyboard_call)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     bot.polling()
